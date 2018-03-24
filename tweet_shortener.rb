9eb0850167d548d0fb9b22071e50bd3a4db04923
Joe Greiner <jrgreiner115@gmail.com>
2018-03-24 23:53:19 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,7 +32,11 @@ def bulk_tweet_shortener(tweet_array)
    end
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
+  if tweet.split <= 140,
+    return tweet
+  else
+    
 end
 
 def shortened_tweet_truncator
