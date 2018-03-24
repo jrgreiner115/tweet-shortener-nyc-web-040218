5894a94fb9a98ad03cd9eab1235707087f96b233
Joe Greiner <jrgreiner115@gmail.com>
2018-03-24 23:47:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,7 +25,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweet_array)
-    tweet_array.each do |tweet|
+    tweet_array.collect do |tweet|
         puts word_substituter(tweet)
    end
 end
