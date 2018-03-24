cd6481dc565bb2f36b8a35b574c3998f398f359c
Joe Greiner <jrgreiner115@gmail.com>
2018-03-24 23:35:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,10 +2,16 @@
 
 def dictionary
   dictionary = {
-    hello: "hi",
-    to
-    
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end
 
+def 
 def bulk_tweet_shortener
