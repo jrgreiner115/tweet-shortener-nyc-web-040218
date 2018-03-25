938ae84a3ee77a20576002640b60c8bd9a7900d9
Joe Greiner <jrgreiner115@gmail.com>
2018-03-25 00:00:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,5 +44,7 @@ def shortened_tweet_truncator(tweet)
   fixed_tweet = word_substituter(tweet)
   if fixed_tweet.length >= 140
    puts "${word_substituter(tweet)[0..136]}..."
+ else
+   fixed_tweet
   end
 end
